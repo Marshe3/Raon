5ec57b6e65d7283b0c1bf8189c1ec6ddafba6d7b
yeoni <mil42856@gmail.com>
2025-10-22 11:49:43 +0900

    test

--- src/main/java/com/example/raon/controller/UserController.java
+++ src/main/java/com/example/raon/controller/UserController.java
@@ -1,7 +1,14 @@
 package com.example.raon.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
-	public static void main(String[] args) {
-        System.out.println("안녕하세요! 라온입니다.");
+    
+    @GetMapping("/")
+    public String home() {
+        return "안녕하세요! 라온입니다.";
     }
+    
 }
